3edf568ca072076f6ea6e6e201f0db4bd819df62
ratulbbz <candyratul67@gmail.com>
2026-08-05 08:11:55 +0600

    Update credits in antiProtect.js

--- Script/events/antiProtect.js
+++ Script/events/antiProtect.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 module.exports.config = {
   name: "antiProtect",
   version: "1.0.0",
-  credits: "SHAHADAT SAHU",
+  credits: "Ratul",
   description: "Protect group name and photo",
   eventType: ["log:thread-name", "log:thread-icon"],
   cooldowns: 3
