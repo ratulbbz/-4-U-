0ff29c1ff51bf396b92d1594a1334d01a03a19bb
ratulbbz <candyratul67@gmail.com>
2026-08-05 09:58:04 +0600

    Update bot credits and admin info

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -2,10 +2,10 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "Ratul",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
-    usages: "",
+    usages: "Ratul",
     cooldowns: 4,
     dependencies: {
         "request": "",
@@ -54,7 +54,7 @@ module.exports.run = async ({ api, event, args }) => {
     if (args[0] == "admin") {
         let callback = () => api.sendMessage(
             {
-                body: `———»ADMIN BOT«———\n❯ Name: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔\n❯ Facebook: https://facebook.com/100044713412032\n❯ Thanks for using ${global.config.BOTNAME} bot`,
+                body: `———»ADMIN BOT«———\n❯ Name: Ratul\n❯ Facebook: https://facebook.com/100044713412032\n❯ Thanks for using ${global.config.BOTNAME} bot`,
                 attachment: fs.createReadStream(__dirname + "/cache/1.png")
             },
             event.threadID,
